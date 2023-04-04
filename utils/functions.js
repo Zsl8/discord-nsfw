3905ec36e05178a06b6f1eb82a0cadb9fa4ff50d
Zsl8 <71377428+Zsl8@users.noreply.github.com>
2023-04-04 17:08:56 +0300

    Update functions.js

--- utils/functions.js
+++ utils/functions.js
@@ -1,8 +1,26 @@
+const Jimp = require('jimp')
+const fs = require('node:fs')
 const fetch = require('node-fetch')
+
 
 module.exports = {
     getBufferImage: async function(url) {
+        let num = Math.floor(Math.random() * 10000000)
+
+        if(!url.includes('jpeg') && !url.includes('png') && !url.includes('bmp')) {
+            Jimp.read(url, (err, image) => {
+                if(err) return;
+                image.write(`./temp/${num}.png`)
+                url = `./temp/${num}.png`
+            })
+        }
+
         const res = await fetch(url);
+
+        setTimeout(() => {
+            fs.unlink(`./temp/${num}.png`, (e) => {})
+        }, 3000)
+
         return res && res.ok ? await res.buffer() : null;
     }
 }
